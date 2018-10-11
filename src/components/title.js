554a3abd1a644b253953f4f7bd8159f95bff83dc
aivey65 <39443120+aivey65@users.noreply.github.com>
2018-10-10 20:27:40 -0500

    trying o=somethign cuz im stuupid?

---
+++ src/components/title.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Title extends React.Component {
+  render() {
+    return <div>{this.props.title}</div>;
+  }
+}
+
+export default Title;
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,8 @@ class Home extends React.Component {
   render() {
     return ( <div>
         <h2>Home</h2>
+        console.log("hi");
+        //<ListItem component = {this.props.todo}></ListItem>
       </div>);
   }
 }
